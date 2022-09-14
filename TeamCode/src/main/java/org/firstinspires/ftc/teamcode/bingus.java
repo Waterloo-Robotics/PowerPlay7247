99665d7273085fdcfe856548acea858dd7632f4c
H2OLooBots <ftcteam7247@gmail.com>
2022-09-13 20:14:15 -0500

    Initial Commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bingus.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp
+public class bingus extends LinearOpMode {
+    public DcMotor motor1;
+    Servo servo1;
+
+    public void runOpMode() {
+        motor1 = hardwareMap.dcMotor.get("cone");
+        servo1 = hardwareMap.servo.get("fan");
+        waitForStart();
+        while (opModeIsActive()) {
+            motor1.setPower(gamepad1.left_stick_x);
+            if (gamepad1.b){
+                servo1.setPosition(0.5);
+            } else {
+                servo1.setPosition(1);
+            }
+        }
+
+    }
+
+}
